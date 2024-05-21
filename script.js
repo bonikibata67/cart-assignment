a6fc9fec0a2e55bf6efd7dcc3b5288ce4af59e8a
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-05-21 22:26:51 +0300

    save the various URLs ganerated in .js file

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log('begin script execution')
 
+// toggle the cart sidebar
 let cartIcon = document.querySelector('#cart-icon')	//grab the cart menu icon
 let sidebar = document.querySelector('.cart-sidebar')	//grab the sidebar
 
@@ -9,3 +10,7 @@ cartIcon.onclick = function() {
 	sidebar.classList.toggle('active');
 	console.log('status changed to active')
 }
+
+// store the database
+const productsURL = 'http://localhost:3000/products'
+const usersURL = 'http://localhost:3000/users'
