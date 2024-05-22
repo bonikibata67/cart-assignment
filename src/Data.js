f36dc463769bc413dc3e3a79413a2d256f52329b
boniface kibata <bonikibata67@gmail.com>
2024-05-22 12:05:48 +0300

    files added

---
+++ src/Data.js
@@ -0,0 +1,56 @@
+let shopItemsData = [{
+    id:"xzdsxdfgh",
+    name:"Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing,",
+    img: "images/Img1.webp"    
+}, 
+{
+    id:"WETYUJ",
+    name:"Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing,",
+    img: "images/Img2.jpg" 
+}, 
+{
+    id:"mzfghjk",
+    name:"T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing,",
+    img: "images/Img3.jpg" 
+},
+{
+    id:"qWERTYU",
+    name:"Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing,",
+    img: "images/Img4.webp" 
+ },
+ {
+    id:"asduwygb",
+    name:"Beninos 3/4 Sleeve",
+    price: 35,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing,",
+    img: "images/Img5.jpg" 
+},
+{
+    id:"suioklmn ",
+    name:"Two Piece Pencil Skirt",
+    price: 47,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing,",
+    img: "images/Img6.jpg" 
+},
+{
+    id:"bnmazxsqw",
+    name:"Zara Pants",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing,",
+    img: "images/Img7.jpg" 
+},
+{
+    id:"ertyuihjjk",
+    name:"Women's suit official wear",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing,",
+    img: "images/Img8.jpg" 
+}]
