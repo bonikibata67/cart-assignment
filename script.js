4a772ea54fd1057fa21c9e24de4efc722bb1563c
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-05-22 14:04:59 +0300

    update the script.js file

--- script.js
+++ script.js
@@ -1,16 +1,86 @@
 console.log('begin script execution')
 
 // toggle the cart sidebar
-let cartIcon = document.querySelector('#cart-icon')	//grab the cart menu icon
-let sidebar = document.querySelector('.cart-sidebar')	//grab the sidebar
+const cartIcon = document.querySelector('#cart-icon')	//grab the cart menu icon
+const sidebar = document.querySelector('.cart-sidebar')	//grab the sidebar
+const closeBtn = document.getElementById('cart-close')
+const mainDiv = document.querySelector('.main-content')
+// const cartBtn = document.querySelector('.add-cart')  //add to cart
+let productHtml = document.querySelector('.card')   //adding html
+// const signBtn= document.getElementById('signup')
+// const loginBtn = document.getElementById('login')
+
+
 
 console.log('got the variables')
 
-cartIcon.onclick = function() {
-	sidebar.classList.toggle('active');
-	console.log('status changed to active')
+closeBtn.addEventListener('click', closeSideBar)    
+cartIcon.addEventListener('click', closeSideBar)
+// signBtn.addEventListener('click', signup)
+// loginBtn.addEventListener('click', login)
+
+
+function closeSideBar(){
+    sidebar.classList.toggle('active');
+    console.log('status changed to active')    
 }
+
+
+// function signup(e){
+//     e.preventDefault()
+//     console.log('signup clicked')    
+// }
+
 
 // store the database
 const productsURL = 'http://localhost:3000/products'
 const usersURL = 'http://localhost:3000/users'
+
+
+//ADDING THE CART FUNCTIONALITY
+// handle the data
+let products = []
+
+function fetchData(){
+    fetch(productsURL).then(response => response.json())
+    .then( data => {
+        products = data
+        //console.log(products)   //to see if data is fetched
+
+        addDataToHtml()
+    })
+}
+
+function addDataToHtml(){
+    let html = ''
+
+    if (products.length > 0){
+        products.forEach( product => {
+            html +=`
+            <div class="card">
+                <div class="image">
+                    <img src=${product.image} alt="Image">
+                </div>
+                    <h2 class="title"> ${product.title} </h2>
+                <div class="price">
+                    <label> Ksh. ${product.price} </label>
+                </div>
+                <div>
+                    <button id="add-cart" onclick="addToCart()" class="add-cart"> Add to Cart </button>
+                </div>
+            </div>`
+        })
+    }
+    mainDiv.innerHTML += html
+
+}
+
+fetchData()
+
+// adding to cart
+
+function addToCart(e){
+    alert('do you want to add item to cart?')
+    let product_id = e.target.parentElement.products.id
+    console.log(product_id )
+}
