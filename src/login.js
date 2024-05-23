7ac6887fedfed40db725a411f74674c1dd5b6ace
Mbugua Douglas <mbuguadouglas17@gmail.com>
2024-05-23 16:26:50 +0300

    try implementation of popup text in plain css

--- src/login.js
+++ src/login.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loginBtn = document.getElementById('login');
     const usersUrl = "http://localhost:3000/users";
-    
+    const indexUrl = "http://127.0.0.1:5500/index.html"
+    const alertDiv = document.querySelector('.alert')
+    const successDiv = document.querySelector('#success')
+    const errorDiv = document.querySelector('#error')
+  
+
     loginBtn.addEventListener('click', async (e) => {
         e.preventDefault();    
         
@@ -13,6 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
             password
         };
 
+        let html = ''
+
         // Assuming you want to check if the user exists in the users list.
         try {
             const response = await fetch(usersUrl);
@@ -21,10 +28,25 @@ document.addEventListener('DOMContentLoaded', () => {
             const user = users.find(user => user.email === email && user.password === password);
             
             if (user) {
-                alert('Login successful');
+                // alert('Login successful');
+
+                // set timer delay to allow viewing of pop notifs
+                alertDiv.style.display = 'flex'
+                successDiv.style.visibility = 'show'
+
+                // window.location.href = indexUrl
+
+                setTimeout(() => {
+
+                    window.location.href = indexUrl
+                },5000)
+            
                 // You can redirect the user to another page or perform some other action here
             } else {
-                alert('Invalid email or password');
+                // set timer delay to allow viewing of pop notifs
+                alertDiv.style.display = 'flex'
+                errorDiv.style.visibility = 'show'
+
             }
         } catch (error) {
             console.error('Error fetching users:', error);
--- src/signup.js
+++ src/signup.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const signBtn = document.getElementById('signup');
     const usersUrl = "http://localhost:3000/users";
+    const indexUrl = "http://127.0.0.1:5500/index.html"
+    const popDiv = document.querySelector('.popup')
+    const successDiv = document.querySelector('#success')
+    const errorDiv = document.querySelector('#error')
     
    
 
@@ -30,9 +34,23 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
             if (response.ok) {
-                console.log("User added successfully:", await response.json());
+                // console.log("User added successfully:", )
+                await response.json()
+
+                // set timer delay to allow viewing of pop notifs
+                alertDiv.style.display = 'flex'
+                successDiv.style.visibility = 'show'
+
+                // window.location.href = indexUrl
+
+                setTimeout(() => {
+
+                    window.location.href = indexUrl
+                },5000)
             } else {
-                console.error("Error adding user:", response.statusText);
+                // console.error("Error adding user:", response.statusText);
+                alertDiv.style.display = 'flex'
+                errorDiv.style.visibility = 'show'
             }
         } catch (error) {
             console.error("Error:", error);
